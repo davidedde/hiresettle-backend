cf57898dd6ea3687122bdad79b6cf5772619ddde
Ayomide <105206834+AvatarMiiDe@users.noreply.github.com>
2026-06-26 03:40:05 +0100

    Update events.module.ts

--- src/modules/events/events.module.ts
+++ src/modules/events/events.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
-import { RetentionSchedulerService } from './retention-scheduler.service';
 import { MilestonesModule } from '../milestones/milestones.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { EngagementsModule } from '../engagements/engagements.module';
+import { WebhooksModule } from '../webhooks/webhooks.module'; // Add this import
 
 @Module({
-  imports: [MilestonesModule, NotificationsModule, EngagementsModule],
-  providers: [EventsService, RetentionSchedulerService],
+  imports: [
+    MilestonesModule,
+    NotificationsModule,
+    EngagementsModule,
+    WebhooksModule, // Add it here
+  ],
   controllers: [EventsController],
+  providers: [EventsService],
+  exports: [EventsService],
 })
 export class EventsModule {}
