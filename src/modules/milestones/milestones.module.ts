674b65e6511332e13d7d85a177aba57f7969307c
Ayomide <105206834+AvatarMiiDe@users.noreply.github.com>
2026-06-26 03:16:00 +0100

    Update milestones.module.ts

--- src/modules/milestones/milestones.module.ts
+++ src/modules/milestones/milestones.module.ts
@@ -1,11 +1,11 @@
-// milestones.module.ts
 import { Module } from '@nestjs/common';
 import { MilestonesController } from './milestones.controller';
 import { MilestonesService } from './milestones.service';
+import { RetentionSchedulerService } from './retention-scheduler.service'; // Add this import
 
 @Module({
   controllers: [MilestonesController],
-  providers: [MilestonesService],
+  providers: [MilestonesService, RetentionSchedulerService], // Add it here
   exports: [MilestonesService],
 })
 export class MilestonesModule {}
